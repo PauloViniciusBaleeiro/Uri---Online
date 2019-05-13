4d42a432123097ca38043d3386639a1fcf373b94
Paulo Vinicius Baleeiro Ferreira <paulo@jumpi.com.br_>
2019-05-13 08:50:07 -0300

    fix bugs

--- git_update.bat
+++ git_update.bat
@@ -1,14 +1,15 @@
-#######################################################################
-# Autor: Paulo Vinícius Baleeiro Ferreira                             #
-# Script destinado a atualização automatizada de repositórios locais  #
-# Para utilização dos funcionários da JUMPI para o projeto Facilitame #
-#                                                                     #
-#######################################################################
+rem#######################################################################
+rem# Autor: Paulo Vinícius Baleeiro Ferreira                             #
+rem# Script destinado a atualização automatizada de repositórios locais  #
+rem# Para utilização dos funcionários da JUMPI para o projeto Facilitame #
+rem#                                                                     #
+rem#######################################################################
 
 
 echo Iniciando o processo de atualização do GIT
 cd c:\
 cd projetos
+cd facilitame
 
 echo Atualizando API Auth
 cd api_facilitame_auth
@@ -34,28 +35,28 @@ echo Atualização concluída
 echo Atualizando Postgre
 cd banco_facilitame_postgre
 git checkout master
-git fetch
+git pull
 cd ..
 echo Atualização concluída
 
 echo Atualizando Frontend Cadastro
 cd frontend_facilitame_cadastro
 git checkout master
-git fetch
+git pull
 cd ..
 echo Atualização concluída
 
 echo Atualizando Frontend Cliente
 cd frontend_facilitame_cliente
 git checkout master
-git fetch
+git pull
 cd ..
 echo Atualização concluída
 
 echo Atualizando Site
 cd site_facilitame
 git checkout master
-git fetch
+git pull
 cd ..
 echo Atualização concluída
 pause
